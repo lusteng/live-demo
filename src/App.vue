159705a51c824d8d0e125748970928e652ef1815
liubaitong <1257782926@qq.com>
2021-03-28 13:28:04 +0800

    feat:支持刷礼物

--- src/App.vue
+++ src/App.vue
@@ -1,97 +1,132 @@
 <template>
   <div id="app">  
-        <video :src="giftUri" style="width: 100px; height: 100px" type="video/mp4" id="giftVideo1"></video>
     <div class="alive">
       <!-- 礼物展示区 -->
       <div class="gift" v-show="showGiftRate">
+        <p class="giftAlert">
+          <em>你打赏了个</em>
+          <span>{{giftWord}}</span>
+          <em>给主播</em>
+        </p>
         <video :src="giftUri" id="giftVideo"></video>
       </div>
       <!-- 礼物展示区 -->
       <!-- 舞台 -->
-      <div class="stage"></div>
+      <div class="stage" v-show="stageShow">
+        <div class="">
+          <div>送出</div>
+        </div>
+      </div>
       <!-- 舞台 -->
       <!-- 直播视频区 -->
-      <div class="video-box"> 
-        <div id="video" class="video"></div>
+      <div class="live-box"> 
+        <div id="liveVideo" class="video"></div>
       </div>
       <!-- 直播视频区 -->
     </div> 
     <!-- 展示条 -->
     <div class="bar"> 
-        <div class="bar-item">
-          <MenuGroup>
-              <template slot="title"> 
-                <ImgCard 
-                  :src="imgs[0]"
-                />
-                统计分析
-              </template>
-              <MenuItem v-for="(uri, index) in imgs" :name="index" :key="index"> 
-              {{uri}}
-                <div class="img-card">
-                    <img src="./assets/1.png" alt=""> 
-                </div>
-              </MenuItem>
-          </MenuGroup> 
+        <div class="bar-item" v-for="(uri, index) in imgs" :key="index">  
+          <ImgCard 
+            :src="uri[0]"
+            :explain="uri[1]"
+            @on-click="handleSentStage"
+          /> 
         </div>
         <div class="bar-item"> 
           <Button @click="handleReward">
-            打赏
-            <!-- 打赏下主播呗 -->
+            打赏下主播呗
           </Button> 
         </div>  
     </div>
-    <!-- 展示条 -->
-    <div @click="onPlay">点我播放</div>
+    <!-- 展示条 --> 
   </div>
 </template>
 
 <script>
   import DPlayer from 'dplayer'
-  import ImgCard from './ImgCard'
+  import ImgCard from './components/ImgCard'
   import './app.css'
-  import { Button, MenuItem, MenuGroup } from 'iview' 
+  import { Button, } from 'iview' 
   const GIFTS = [
-    '/static/source/1.mp4',
-    '/static/source/2.mp4',
-    '/static/source/3.mp4',
-    '/static/source/4.mp4',
+    require('/static/source/1.mp4'),
+    require('/static/source/2.mp4'),
+    require('/static/source/3.mp4'),
+    require('/static/source/4.mp4'),
   ] 
+  const WS = [
+    '红色大跑车',
+    '飞龙在天',
+    '森林精灵',
+    '金色麒麟',
+  ]
   
   export default {
     name: "optimizeVideo",
     components: { 
-      Button,
-      MenuItem,
-      MenuGroup,
-      ImgCard,
+      Button, 
+      ImgCard, 
     },
     data() {
       return {
         dp:null,
         giftUri: GIFTS[0],
-        showGiftRate: true,
+        showGiftRate: false,
+        stageShow: false,
+        stagImg: "",
+        stagExplain: "",
+        giftWord: WS[0],
         imgs: [
-          './../static/source/1.png',
-          './../static/source/2.png',
-          './../static/source/3.png',
-          './../static/source/4.png',
+          [require('/static/source/1.png'), '小花花'],
+          [require('/static/source/2.png'), '小皇冠'],
+          [require('/static/source/3.png'), '小火箭'],
+          [require('/static/source/4.png'), '小心心'],
         ]
       }
     },
     methods:{
-      handleReward(){
-        document.getElementById('giftVideo').src = GIFTS[Math.floor(Math.random() * 3)]
-        console.log(document.getElementById('giftVideo'));
-
-        document.getElementById('giftVideo').play() 
+      handleSentStage(o){ 
+        console.log(src);
+        let {
+          src, explain
+        } =  o
+        this.stagExplain = explain
+        this.stagImg = src
       },
-      onPlay() {
-          this.dp.play()
-      }
-    }, 
-    created () {
+      handleReward(){
+        let random = Math.floor(Math.random() * 4)
+        document.getElementById('giftVideo').src = GIFTS[random]
+        this.giftWord = WS[random] 
+        document.getElementById('giftVideo').play() 
+      }, 
+    },  
+    mounted() { 
       this.$nextTick(() => {
+        this.dp = new DPlayer({
+          // 配置参数
+          container: document.getElementById('liveVideo'),
+          autoplay: true,
+          theme: 'blue',  
+          volume: 0.2,
+          video: {
+            url: './../live.flv', 
+            type: 'flv',
+          },
+        });
+
+        this.dp.play()
+        // 禁止右键下载视频
+        document.oncontextmenu = new Function("event.returnValue=false;");
+        document.onselectstart = new Function("event.returnValue=false;");
+
+        // 修改循环播放显示
+        document.getElementsByClassName('dplayer-setting-item dplayer-setting-loop')[0].getElementsByClassName('dplayer-label')[0].innerText = "循环播放"
+        // 修改倍速播放显示
+        document.getElementsByClassName('dplayer-setting-item dplayer-setting-speed')[0].getElementsByClassName('dplayer-label')[0].innerText = "播放倍速"
+
+
+
+        
         document.getElementById('giftVideo').addEventListener('play', () => {
           this.showGiftRate = true
         })
@@ -99,34 +134,6 @@
           this.showGiftRate = false
         })
       })
-    },
-    mounted() {
-      console.log(GIFTS);
-      this.dp = new DPlayer({
-        // 配置参数
-        container: document.getElementById('video'),
-        autoplay: false,
-        theme: '#FADFA3',
-        loop: true,
-        lang: 'zh-cn',
-        preload: 'auto',
-        // logo: 'logo.png',
-        volume: 0.7,
-        video: {
-          url: './../source/live.flv',
-          pic: 'dplayer.png',
-          type: 'flv',
-        },
-      });
-      
-      // 禁止右键下载视频
-      document.oncontextmenu = new Function("event.returnValue=false;");
-      document.onselectstart = new Function("event.returnValue=false;");
-
-      // 修改循环播放显示
-      document.getElementsByClassName('dplayer-setting-item dplayer-setting-loop')[0].getElementsByClassName('dplayer-label')[0].innerText = "循环播放"
-      // 修改倍速播放显示
-      document.getElementsByClassName('dplayer-setting-item dplayer-setting-speed')[0].getElementsByClassName('dplayer-label')[0].innerText = "播放倍速"
     }
   }
 </script>
---
+++ src/components/ImgCard.vue
@@ -0,0 +1,41 @@
+<template>
+    <div 
+        class="img-card"
+        @click="$emit('on-click', {src, explain})"
+    >
+        <img :src="src" alt=""> 
+    </div>
+</template>
+
+<script>
+    export default {
+        name:"ImgCard",
+        props: {
+            explain: String,
+            src: {
+                type: String,
+                default: "",
+                require: true,
+            },
+        },
+    }
+</script>
+
+<style>
+.img-card img{
+    box-sizing: border-box;
+    width: 40px;
+    height: 40px;
+    vertical-align: middle;
+    padding: 3px;
+    border: 1px solid #ddd;
+    margin-right:5px; 
+    border-radius: 5px;
+    transition: .3s;
+    cursor: pointer;
+}
+.img-card img:hover{
+    border-color: #f40;
+}
+
+</style>
